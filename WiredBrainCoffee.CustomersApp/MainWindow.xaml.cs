f43ef0b05fa3827f2fcea6002753524f44ae3321
iaguest <i.guest2010@gmail.com>
2023-02-19 20:22:41 +0000

    Fix layout. Allow moving of nav window by adjusting attached props.

--- WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
+++ WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WiredBrainCoffee.CustomersApp
 {
@@ -23,6 +11,18 @@ namespace WiredBrainCoffee.CustomersApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            //var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            //var newColumn = column == 0 ? 2 : 0;
+            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            var column = Grid.GetColumn(customerListGrid);
+
+            var newColumn = column == 0 ? 2 : 0;
+            Grid.SetColumn(customerListGrid, newColumn);
         }
     }
 }
