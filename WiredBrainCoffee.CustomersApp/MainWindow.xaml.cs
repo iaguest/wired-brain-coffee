f1e994226943ee8d24f49bb9ce3fde0a109a6d86
iaguest <i.guest2010@gmail.com>
2023-02-24 21:02:20 +0000

    working with data templates

--- WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
+++ WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
 
 namespace WiredBrainCoffee.CustomersApp
 {
@@ -8,9 +9,21 @@ namespace WiredBrainCoffee.CustomersApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly MainViewModel _viewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+            _viewModel = new MainViewModel(
+                new CustomersViewModel(new CustomerDataProvider()),
+                new ProductsViewModel());
+            DataContext = _viewModel;
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.LoadAsync();
         }
     }
 }
