23c45f84258caaead4789b68eed747e6c00f5a86
iaguest <i.guest2010@gmail.com>
2023-02-25 13:45:55 +0000

    App styling

--- WiredBrainCoffee.CustomersApp/App.xaml.cs
+++ WiredBrainCoffee.CustomersApp/App.xaml.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Windows;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
 
 namespace WiredBrainCoffee.CustomersApp
 {
@@ -13,5 +11,33 @@ namespace WiredBrainCoffee.CustomersApp
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services= new();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient<MainWindow>();
+
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<CustomersViewModel>();
+            services.AddTransient<ProductsViewModel>();
+
+            services.AddTransient<ICustomerDataProvider, CustomerDataProvider>();
+            services.AddTransient<IProductDataProvider, ProductDataProvider>();
+        }
     }
 }
